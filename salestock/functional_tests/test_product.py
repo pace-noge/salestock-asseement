b0d1cba46c0b11d146a6fdfac979e53b3494fa8f
nasa <nasa.freaks@gmail.com>
2016-07-14 15:56:28 +0700

    replace category word to product

--- salestock/functional_tests/test_product.py
+++ salestock/functional_tests/test_product.py
@@ -3,71 +3,71 @@ from django.core.urlresolvers import reverse
 import json
 from rest_framework.test import force_authenticate
 from rest_framework import status
-from products.models import Category
+from products.models import Product
 
 
 
-class CategoryTest(FunctionalTest):
+class ProductTest(FunctionalTest):
 
-    def test_get_list_of_category(self):
-        self.create_category()
+    def test_get_list_of_product(self):
+        self.create_product()
         self.browser.get(self.server_url+"/categories/?format=json")
         self.take_screenshot()
         result = json.loads(self.browser.find_element_by_tag_name("body").text)
-        self.assertEqual(result[0]["title"], "test category")
+        self.assertEqual(result[0]["title"], "test product")
         self.assertEqual(len(result), 1)
 
-    def test_create_category_with_login(self):
+    def test_create_product_with_login(self):
         """
-        Ensure we can create new category via rest framework client
+        Ensure we can create new product via rest framework client
         """
-        url = reverse("category-list")
+        url = reverse("product-list")
         data = {
-            "title": "second category",
-            "slug": "second-category",
-            "description": "second category description"
+            "title": "second product",
+            "slug": "second-product",
+            "description": "second product description"
         }
         self.client.force_authenticate(user=self.create_user())
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Category.objects.get().title, "second category")
+        self.assertEqual(Product.objects.get().title, "second product")
         self.browser.get(self.server_url+"/categories/?format=json")
         self.take_screenshot()
         result = json.loads(self.browser.find_element_by_tag_name("body").text)
-        self.assertEqual(result[0]["title"], "second category")
+        self.assertEqual(result[0]["title"], "second product")
         self.assertEqual(len(result), 1)
 
-    def test_create_category_without_login(self):
-        url = reverse("category-list")
+    def test_create_product_without_login(self):
+        url = reverse("product-list")
         data = {
-            "title": "second category",
-            "slug": "second-category",
-            "description": "second category description"
+            "title": "second product",
+            "slug": "second-product",
+            "description": "second product description"
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_update_category(self):
-        self.create_category()
-        url = reverse("category-detail", kwargs={'slug': 'test-category'})
+    def test_update_product(self):
+        self.create_product()
+        url = reverse("product-detail", kwargs={'slug': 'test-product'})
         data = {
-            "title": "second category",
-            "description": "second category description"
+            "title": "second product",
+            "description": "second product description"
         }
         self.client.force_authenticate(user=self.create_user())
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Category.objects.get().title, "second category")
+        self.assertEqual(product.objects.get().title, "second product")
         self.browser.get(self.server_url+"/categories/?format=json")
         self.take_screenshot()
         result = json.loads(self.browser.find_element_by_tag_name("body").text)
-        self.assertEqual(result[0]["title"], "second category")
+        self.assertEqual(result[0]["title"], "second product")
         self.assertEqual(len(result), 1)
 
-    def test_delete_category(self):
-        self.create_category()
-        self.assertEqual(Category.objects.count(), 1)
-        url = reverse("category-detail", kwargs={'slug': 'test-category'})
+    def test_delete_product(self):
+        self.create_product()
+        self.assertEqual(product.objects.count(), 1)
+        url = reverse("product-detail", kwargs={'slug': 'test-product'})
         self.client.force_authenticate(user=self.create_user())
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
@@ -76,14 +76,14 @@ class CategoryTest(FunctionalTest):
         result = json.loads(self.browser.find_element_by_tag_name("body").text)
         self.assertEqual(len(result), 0)
 
-    def test_delete_category_without_login(self):
-        self.create_category()
-        self.assertEqual(Category.objects.count(), 1)
-        url = reverse("category-detail", kwargs={'slug': 'test-category'})
+    def test_delete_product_without_login(self):
+        self.create_product()
+        self.assertEqual(product.objects.count(), 1)
+        url = reverse("product-detail", kwargs={'slug': 'test-product'})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         response.render()
-        self.assertEqual(Category.objects.count(), 1)
+        self.assertEqual(product.objects.count(), 1)
         self.browser.get(self.server_url+"/categories/?format=json")
         self.take_screenshot()
         result = json.loads(self.browser.find_element_by_tag_name("body").text)
